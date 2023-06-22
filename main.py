411db706e4c2c411402622131adb8b895caf7c29
patrick-nelissen <pnelissen@yahoo.com>
2023-06-21 19:42:56 -0700

    Added buttons for on and off instead of need for web-url

--- main.py
+++ main.py
@@ -8,86 +8,90 @@ import _thread
 
 from EngineLED import EngineLED
 
+def webpage():
+    html = f"""
+            <!DOCTYPE html>
+            <html>          
+            <body>
+            <form action="./on">
+            <input type="submit" value="Engine On"/>
+            </form>
+            </form>
+            <form action="./off">
+            <input type="submit" value="Engine Off" />
+            </form>
+            </body>
+            </html>
+            """
+
+    return html
+ 
+def serve(connection):
+    while True:
+        client = connection.accept()[0]
+        request = client.recv(1024)
+        request = str(request)
+        try:
+            request = request.split()[1]
+        except IndexError:
+            pass
+        
+        print(request)
+        
+        if request == '/on?':
+            EngineOn = True
+        elif request == '/off?':
+            EngineOn = False      
+  
+        html=webpage()
+        client.send(html)
+        client.close()
+ 
+def open_socket(ip):
+    # Open a socket
+    address = (ip, 80)
+    connection = socket.socket()
+    connection.bind(address)
+    connection.listen(1)
+    print(connection)
+    return(connection)
+
+
+
 # The Pi Pico has 2 cores, core 0 and core 1
 # This core, core 0 will run the webservice
 def core0_thread():
     
     global EngineOn
 
-    ssid = 'your_ssid'
-    password = 'your_password'
-
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
-    wlan.connect(ssid, password)
-
-    html = """<!DOCTYPE html>
-    <html>
-        <head> <title>Saturn V - Engine</title> </head>
-        <body> <h1>Saturn V - Engine</h1>
-            <p>%s</p>
-        </body>
-    </html>
-    """
-
+    wlan.connect("your_SSID","your_PASSWORD")
+         
     # Wait for connect or fail
-    max_wait = 10
-    while max_wait > 0:
+    wait = 10
+    while wait > 0:
         if wlan.status() < 0 or wlan.status() >= 3:
             break
-        max_wait -= 1
+        wait -= 1
         print('waiting for connection...')
         time.sleep(1)
-
+     
     # Handle connection error
     if wlan.status() != 3:
-        raise RuntimeError('network connection failed')
+        raise RuntimeError('wifi connection failed')
     else:
         print('connected')
-        status = wlan.ifconfig()
-        print( 'ip = ' + status[0] )
-
-    # Open socket
-    addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
-    s = socket.socket()
-    s.bind(addr)
-    s.listen(1)
-
-    print('listening on', addr)
-
-    # Listen for connections
-    while True:
-        try:
-            cl, addr = s.accept()
-            print('client connected from', addr)
-            request = cl.recv(1024)
-            print(request)
-            request = str(request)
-            engine_on = request.find('/engine/on')
-            engine_off = request.find('/engine/off')
-            print( 'Engine on = ' + str(engine_on))
-            print( 'Engine off = ' + str(engine_off))
-
-            if engine_on == 6:
-                print("Engine on")
-                stateis = "Engine is ON"
-                EngineOn = True
-                    
-            if engine_off == 6:
-                print("Engine off")
-                stateis = "Engine is OFF"
-                EngineOn = False
-
-
-            response = html % stateis
-            cl.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
-            cl.send(response)
-            cl.close()           
-
-
-        except OSError as e:
-            cl.close()
-            print('connection closed')
+        ip=wlan.ifconfig()[0]
+        print('IP: ', ip)
+        
+    try:
+        if ip is not None:
+            connection=open_socket(ip)
+            serve(connection)
+    except KeyboardInterrupt:
+        machine.reset()
+        
 
 # The Pi Pico has 2 cores, core 0 and core 1
 # This core, core 1 will drive the LEDs and randomly
